daf48ac559377764bdcd320bd0f3c6acc79b0656
Admin <ermix.fiorillo@gmail.com>
2025-04-01 22:28:39 +0200

    Background image

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,8 +6,8 @@ import "../styles/Intro.css";
 const Intro = () => (
   <section id="intro">
     <div className="intro-content">
-      <h1>Hi, I'm Ermanno</h1>
-      <p>Welcome to my portfolio</p>
+      <h1>Hi, Ermanno here!</h1>
+      <p>Welcome to my Portfolio</p>
       <OrganicBrainSketch />
     </div>
   </section>
