cf0659d97b91f46dc3a8d6153394599027abdbe4
Ermanno Fiorillo <132562360+ErmannoF00@users.noreply.github.com>
2025-04-02 12:13:21 +0200

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,7 +8,7 @@ const Intro = () => (
     <div className="intro-content">
       <h1>Hi, Ermanno here!</h1>
       <blockquote className="intro-quote">
-        <em>“The best ideas start with ‘what if?’.”</em>
+        <em>-The best ideas start with "what if?"-</em>
       </blockquote>
       <OrganicBrainSketch />
     </div>
