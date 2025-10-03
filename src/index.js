134447d9336721514106aac6faff26834ac285e8
ErmannoF00 <ermix.fiorillo@gmail.com>
2025-10-03 13:49:04 +0200

    HashRouter for page reload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -18,14 +18,12 @@ import "./styles/Projects.css";
 import "./styles/BrainSketch.css";
 import "./styles/Volunteering.css";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* GitHub Pages base path */}
-    <BrowserRouter basename="/Portfolio">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
